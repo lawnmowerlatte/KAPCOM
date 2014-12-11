373c200fc6517aaf5511af55058c1ca38cfdd536
lawnmowerlatte <lawnmowerlatte@gmail.com>
2014-12-11 14:58:32 -0500

    WIP: Arduino sketch
    - Integrated ArduinoJson to the point where it compiles
    - Added in serial listener
    - Started playing around with calibration routine
    - Added comments
    
    Status: Compiles, loads, staging button doesn’t work to end calibration
    routine. Not sure if the problem is hardware.

--- arduino/KAPCOM.ino
+++ arduino/KAPCOM.ino
@@ -6,43 +6,128 @@
 #include <pinio.h>
 #include <lockedinput.h>
 #include <joy.h>
+#include <ArduinoJson.h>
 
+// Global Serial Reading
+char* readLine;
+char* nextLine;
+
+// Create joystick objects
 Joy J0("J0", A0, A1, A2);
 Joy J1("J1", A4, A5, A6);
 
-Pin soft("Soft", 54+4, DIGITAL, INPUT);
-PinIO rcs("RCS", 1, 2);
-LockedInput stage("Stage", 3, 4, 5, 6);
-LockedInput abandon("Abort", 7, 8, 9, 10);
+// Create button objects
+Pin soft("Soft", "?", 54+4, DIGITAL, INPUT);
+PinIO rcs("RCS", "rcs", 1, 2);
+LockedInput stage("Stage", "stage", 1, 2, 3, 4);
+LockedInput abandon("Abort", "abort", 7, 8, 9, 10);
 
 void setup() {
+  // Start the serial connection
   Serial.begin(9600);
+  // Report online status
+  Serial.println("ONLINE");
+  // Receive configuration
+  // configure();
   
-  Serial.println("Calibrating")
   
-  while (stage.button.get() == LOW) {
-   J0.calibrate();
-   J1.calibrate(); 
+  // Report calibration status
+  Serial.println("CALIBRATING");
+  
+  stage.button.out.set(HIGH);
+  
+  // Calibrate until the staging button is pressed
+  while (1) {
+   //Serial.println(stage.button.in.get());
+   Serial.println(digitalRead(3));
+   //J0.calibrate();
+   //J1.calibrate();
   }
   
-  Serial.println("Ready")
+  // Report ready status
+  Serial.println("READY");
 }
 
-void loop() {
+void(* reset) (void) = 0;
+
+   
+
+
+void serialEvent() {
+  while (Serial.available()) {
+    char c = (char)Serial.read();
+    
+    if (c == '\n') {
+      readLine = nextLine;
+      nextLine ="";
+    } else {
+      nextLine += c;
+    }
+  }
+}
+
+void configure() {
+  StaticJsonBuffer<512> jsonBuffer;
+  char* json = readLine;
+  
+  // Parse JSON
+  JsonObject& configuration = jsonBuffer.parseObject(json);
+  if (!configuration.success()) {
+    Serial.println("JSON Parsing failed.");
+    reset();
+  }
+  
+}
+
+void poll() {
+  // Poll all hardware inputs for latest values
   soft.update();
   rcs.update();
   J0.update();
   J1.update();
+}
+
+void processInput() {
+  StaticJsonBuffer<512> jsonBuffer;
+  JsonObject& input = jsonBuffer.createObject();
   
-  J0.print();
-  J1.print();
+  // Aggregate fly-by-wire data
   
-  String output = "I: " + String(J0.X, HEX) + String(J0.Y, HEX) + String(J0.Z, HEX) + String(J1.X, HEX) + String(J1.Y, HEX) + String(J1.Z, HEX);
-  output.toUpperCase();
   
-  Serial.println(output);
-  //soft.print();
-  //rcs.print();
   
-  //delay(1000);
+  // Send fly-by-wire data
+  input.printTo(Serial);
+}
+
+void processOutput() {
+  StaticJsonBuffer<512> jsonBuffer;
+  char* json = readLine;
+  
+  // Parse received telemetry
+  JsonObject& output = jsonBuffer.parseObject(json);
+  if (!output.success()) {
+    Serial.println("JSON Parsing failed.");
+    reset();
+  }
+  
+  // Update instrumentation panels
+  
+  
+}
+
+
+
+void loop() {
+  Serial.println("loop()");
+  delay(100000000);
+  
+  
+  
+  
+  // Wait for telemetry data
+  processOutput();
+  // Poll hardware for inputs
+  poll();
+  // Send fly-by-wire data
+  processInput();
 }

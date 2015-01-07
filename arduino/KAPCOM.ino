e5275e3c6d71d6c1302e4e021f9c40121ac39740
lawnmowerlatte <lawnmowerlatte@gmail.com>
2015-01-07 01:20:11 -0500

    Semi-working!
    Implemented vectors for holding joysticks, buttons, indicators, locked
    inputs and more. Now the sketch can easily iterate over all of them,
    find the changed items and add them to the JSON object. As is, it
    should be able to start talking to Python.

--- arduino/KAPCOM.ino
+++ arduino/KAPCOM.ino
@@ -2,67 +2,105 @@
 #define DIGITAL 0
 #define NULL 0
 
+#include <new.cpp>
+#include <iterator>
+#include <vector>
+
 #include <pin.h>
-#include <pinio.h>
 #include <lockedinput.h>
 #include <joy.h>
 #include <ArduinoJson.h>
+
+using namespace std;
 
 // Global Serial Reading
 char* readLine;
 char* nextLine;
 
-// Create joystick objects
-Joy J0("J0", A0, A1, A2);
-Joy J1("J1", A4, A5, A6);
+// Create calibration inputs
+LockedInput calibrate("Calibrate", "calibrate", A11, A12, A13);
 
-// Create button objects
-Pin soft("Soft", "?", A3, DIGITAL, INPUT_PULLUP);
-Pin hard("Hard", "?", A7, DIGITAL, INPUT_PULLUP);
-LockedInput stage("Stage", "stage", A11, A12, A13);
-LockedInput abandon("Abort", "abort", A8, A9, A10);
+// Create vectors and iterators
+vector<Joy> joys;
+vector<Joy>::iterator joy;
 
+vector<Pin> buttons;
+vector<Pin>::iterator button;
+
+vector<Pin> indicators;
+vector<Pin>::iterator indicator;
+   
+vector<LockedInput> locks;
+vector<LockedInput>::iterator lock;
+/*
+vector<Bargraph> bargraphs;
+vector<Bargraph>::const_iterator bargraph;
+   
+vector<Display> displays;
+vector<Display>::const_iterator display;
+*/
 void setup() {
+  joys.reserve(2);
+  buttons.reserve(20);
+  indicators.reserve(20);
+  locks.reserve(2);
+  /*
+  bargraphs.reserve(5);
+  displays.reserve(4);
+  */
+  // Load joysticks into vector
+  joys.push_back(Joy("J0", A0, A1, A2));
+  joys.push_back(Joy("J1", A4, A5, A6));
+  
+  // Load buttons into vector.*
+  buttons.push_back(Pin("Soft", "?", A3, DIGITAL, INPUT_PULLUP));
+  buttons.push_back(Pin("Trim", "?", A7, DIGITAL, INPUT_PULLUP));
+   
+  // Load locked inputs into vector
+  locks.push_back(LockedInput("Stage", "stage", A11, A12, A13));
+  locks.push_back(LockedInput("Abort", "abort", A8, A9, A10));
+
   // Start the serial connection
   Serial.begin(9600);
   // Report online status
   Serial.println("ONLINE");
   // Receive configuration
   // configure();
-  
+
   // Report calibration status
   Serial.println("CALIBRATING");
-  
-  
+
   // Calibrate until the staging button is pressed
-  stage.indicator.set(HIGH);
-  J0.recalibrate();
-  J1.recalibrate();
-  J0.print();
-  J1.print();
-  while (stage.button.get() == LOW) {
-    J0.calibrate();
-    J1.calibrate();
-  }
-  stage.indicator.set(LOW);
+  calibrate.indicator.set(HIGH);
   
+  for (joy=joys.begin(); joy!=joys.end(); joy++) {
+    joy->recalibrate();
+    //joy->print();
+  }
+
+  while (calibrate.button.get() == LOW) {
+    for(joy=joys.begin(); joy!=joys.end(); joy++) {
+      joy->calibrate();
+    }
+  }
+
+  calibrate.indicator.set(LOW);
+
   // Report ready status
   Serial.println("READY");
 }
 
 void(* reset) (void) = 0;
 
-   
-
-
 void serialEvent() {
   while (Serial.available()) {
     char c = (char)Serial.read();
-    
+
     if (c == '\n') {
       readLine = nextLine;
       nextLine ="";
-    } else {
+    } 
+    else {
       nextLine += c;
     }
   }
@@ -71,81 +109,115 @@ void serialEvent() {
 void configure() {
   StaticJsonBuffer<512> jsonBuffer;
   char* json = readLine;
-  
+
   // Parse JSON
   JsonObject& configuration = jsonBuffer.parseObject(json);
   if (!configuration.success()) {
     Serial.println("JSON Parsing failed.");
     reset();
   }
-  
+
 }
 
 void poll() {
-  // Poll all hardware inputs for latest values
-  //soft.update();
-  //rcs.update();
-  J0.update();
-  J1.update();
+  // Poll all configured hardware 
   
-  //if (stage.updated()) { Serial.println(stage.toJSON()); }
+  for (joy=joys.begin(); joy!=joys.end(); joy++) {
+    joy->update();
+  }
+  
+  for (button=buttons.begin(); button!=buttons.end(); button++) {
+    button->update();
+  }
+   
+  for (lock=locks.begin(); lock!=locks.end(); lock++) {
+    lock->update();
+  }
+   
+  /*
+  for (bargraph=bargraphs.begin(); bargraph!=bargraphs.end(); bargraph++) {
+    bargraph.update();
+  }
+   
+  for (display=displays.begin(); display!=displays.end(); display++) {
+    display.update();
+  }
+  */
 }
 
 void processInput() {
   StaticJsonBuffer<512> jsonBuffer;
   JsonObject& input = jsonBuffer.createObject();
+  String yaw, pitch, roll, x, y, z, sixdof;
   
   // Aggregate fly-by-wire data
   
+  for (joy=joys.begin(); joy!=joys.end(); joy++) {
+    if (joy->name == "J0") {
+      yaw = String(joy->X);
+      pitch = String(joy->Y);
+      roll = String(joy->Z);
+    } else if (joy->name == "J1") {
+      x = String(joy->X);
+      y = String(joy->Y);
+      z = String(joy->Z);
+    }
+  }
+  sixdof=yaw + ", " + pitch + ", " + roll + ", " + x + ", " + y + ", " + z;
+  input["toggle_fbw"] = "1";
+  input["six_dof"] = sixdof.c_str();
   
+  for (button=buttons.begin(); button!=buttons.end(); button++) {
+   if (button->updated()) {
+     input.add(button->api.c_str(), button->get());
+   }
+  }
+  
+  for (lock=locks.begin(); lock!=locks.end(); lock++) {
+   if (lock->updated()) {
+     input.add(lock->api.c_str(), lock->get());
+   }
+  }
   
   // Send fly-by-wire data
   input.printTo(Serial);
+  Serial.println("");
 }
 
 void processOutput() {
   StaticJsonBuffer<512> jsonBuffer;
   char* json = readLine;
-  
+
   // Parse received telemetry
   JsonObject& output = jsonBuffer.parseObject(json);
   if (!output.success()) {
     Serial.println("JSON Parsing failed.");
     reset();
   }
-  
+
   // Update instrumentation panels
-  
-  
+  /*
+  for (bargraph=bargraphs.begin(); bargraph!=bargraphs.end(); bargraph++) {
+   bargraph.set(??);
+   }
+   
+   for (display=displays.begin(); display!=displays.end(); display++) {
+   display.set(??);
+   }
+   */
 }
 
 
 
 void loop() {
-  //Serial.println("loop()");
-  stage.update();
-  abandon.update();
-  J0.update();
-  J1.update();
-  hard.update();
-  soft.update();
-  
-  //if (stage.updated()) { stage.print(); }
-  //if (abandon.update()) { abandon.print(); }
-  //J0.print();
-  //J1.print();
-  
   delay(1000);
-  hard.print();
-  soft.print();
-  
-  
-  
-  
+
+
   // Wait for telemetry data
   //processOutput();
   // Poll hardware for inputs
-  //poll();
+  poll();
   // Send fly-by-wire data
-  //processInput();
+  processInput();
 }
+

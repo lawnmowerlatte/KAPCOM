cfe7bd6815994c09a1a6638b7cd7959863355259
lawnmowerlatte <lawnmowerlatte@gmail.com>
2015-01-08 01:30:46 -0500

    Sketch changes
    Added logic for displays. Updated serialEvent() to be more reliable.
    Still very much a work in progress, but we’re getting closer. This is
    actually able to sync up with the Python program.

--- arduino/KAPCOM.ino
+++ arduino/KAPCOM.ino
@@ -6,19 +6,26 @@
 #include <iterator>
 #include <vector>
 
+#include <LedControl.h>
+
 #include <pin.h>
 #include <lockedinput.h>
 #include <joy.h>
+#include <display.h>
 #include <ArduinoJson.h>
 
 using namespace std;
 
 // Global Serial Reading
-char* readLine;
-char* nextLine;
+String readLine = "";
+String nextLine = "";
 
 // Create calibration inputs
 LockedInput calibrate("Calibrate", "calibrate", A11, A12, A13);
+
+// Create global LedControl
+int display_count = 5;
+LedControl lc(7, 5, 6, display_count);
 
 // Create vectors and iterators
 vector<Joy> joys;
@@ -35,19 +42,28 @@ vector<LockedInput>::iterator lock;
 /*
 vector<Bargraph> bargraphs;
 vector<Bargraph>::const_iterator bargraph;
-   
+
 vector<Display> displays;
 vector<Display>::const_iterator display;
 */
+
 void setup() {
+  // Initialize displays
+  for(int i=0; i<display_count; i++) {
+    lc.shutdown(i,false); // Enable display
+    lc.setIntensity(i,15); // Set brightness level (0 is min, 15 is max)
+    lc.clearDisplay(i);
+  }
+  
   joys.reserve(2);
   buttons.reserve(20);
   indicators.reserve(20);
   locks.reserve(2);
   /*
   bargraphs.reserve(5);
-  displays.reserve(4);
+  displays.reserve(5);
   */
+  
   // Load joysticks into vector
   joys.push_back(Joy("J0", A0, A1, A2));
   joys.push_back(Joy("J1", A4, A5, A6));
@@ -59,7 +75,14 @@ void setup() {
   // Load locked inputs into vector
   locks.push_back(LockedInput("Stage", "stage", A11, A12, A13));
   locks.push_back(LockedInput("Abort", "abort", A8, A9, A10));
-
+  /*
+  displays.push_back(Display("Ap", "vessel_apoapsis", lc, 8, 1));
+  displays.push_back(Display("Pe", "vessel_periapsis", lc, 8, 2));
+  displays.push_back(Display("Alt", "vessel_altitude", lc, 8, 3));
+  displays.push_back(Display("Vel", "vessel_velocity", lc, 8, 4));
+  displays.push_back(Display("Rad", "vessel_asl_height", lc, 4, 5, 0, 3, " "));
+  displays.push_back(Display("Inc", "vessel_inclination", lc, 4, 5, 4, 3, " "));
+  */
   // Start the serial connection
   Serial.begin(9600);
   // Report online status
@@ -95,23 +118,28 @@ void(* reset) (void) = 0;
 void serialEvent() {
   while (Serial.available()) {
     char c = (char)Serial.read();
-
-    if (c == '\n') {
-      readLine = nextLine;
-      nextLine ="";
-    } 
-    else {
-      nextLine += c;
+    
+    nextLine += c;
+    
+    if (c == '\n' || c == 10 || c == 13) {
+      readLine = nextLine.substring(0, nextLine.length() -1);
+      nextLine = "";
+      
+      if (readLine == "RESTART") {
+        Serial.println("Resetting...");
+        delay(1000); 
+        reset();
+      }
     }
   }
 }
 
 void configure() {
   StaticJsonBuffer<512> jsonBuffer;
-  char* json = readLine;
+  char* json; // = readLine.c_str();
 
   // Parse JSON
-  JsonObject& configuration = jsonBuffer.parseObject(json);
+JsonObject& configuration = jsonBuffer.parseObject(json);
   if (!configuration.success()) {
     Serial.println("JSON Parsing failed.");
     reset();
@@ -138,9 +166,9 @@ void poll() {
   for (bargraph=bargraphs.begin(); bargraph!=bargraphs.end(); bargraph++) {
     bargraph.update();
   }
-   
+  
   for (display=displays.begin(); display!=displays.end(); display++) {
-    display.update();
+    display->update();
   }
   */
 }
@@ -179,6 +207,8 @@ void processInput() {
    }
   }
   
+  input.remove("?");
+  
   // Send fly-by-wire data
   input.printTo(Serial);
   Serial.println("");
@@ -186,7 +216,7 @@ void processInput() {
 
 void processOutput() {
   StaticJsonBuffer<512> jsonBuffer;
-  char* json = readLine;
+  char* json; // = readLine;
 
   // Parse received telemetry
   JsonObject& output = jsonBuffer.parseObject(json);
@@ -198,26 +228,36 @@ void processOutput() {
   // Update instrumentation panels
   /*
   for (bargraph=bargraphs.begin(); bargraph!=bargraphs.end(); bargraph++) {
-   bargraph.set(??);
-   }
-   
-   for (display=displays.begin(); display!=displays.end(); display++) {
-   display.set(??);
-   }
-   */
+    bargraph.set(??);
+  }
+  */ /*
+  for (display=displays.begin(); display!=displays.end(); display++) {
+    display->set("1234.5678");
+  }
+  */
 }
 
 
 
 void loop() {
   delay(1000);
-
-
-  // Wait for telemetry data
-  //processOutput();
+  
+  //Serial.println(readLine);
+  if (readLine != "") {
+    // Wait for telemetry data
+    //processOutput();
+  }
+  
   // Poll hardware for inputs
   poll();
-  // Send fly-by-wire data
   processInput();
+  
+  if (readLine != "") { 
+    // Send fly-by-wire data
+    processInput();
+    
+    readLine = "";
+  }
+ 
 }
 

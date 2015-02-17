e6d4e27a53afdf18cc400184e08631a94ab990bd
lawnmowerlatte <lawnmowerlatte@gmail.com>
2015-02-17 00:07:57 -0500

    Bargraph changes
    Added basic implementation of bargraph driver.

--- arduino/libraries/Bargraph/bargraph.cpp
+++ arduino/libraries/Bargraph/bargraph.cpp
@@ -1,7 +1,11 @@
-#define ANALOG 1
-#define DIGITAL 0
-#define NULL 0
+#define OFF 0
+#define RED 1
+#define YELLOW 2
+#define GREEN 3
 
+#include <Wire.h>
+#include "Adafruit_LEDBackpack.h"
+#include "Adafruit_GFX.h"
 #include <bargraph.h>
 
 #if (ARDUINO >= 100)
@@ -14,21 +18,20 @@
 //	Constructors and Destructors
 // =================================
 
-Bargraph::Bargraph(String _name, String _api, String _max, int _device) {
-	name		= _name;
-	api			= _api;
-	max			= _max;
-	device		= _device;
-	
-	type		= "Default";
-}
-
 Bargraph::Bargraph(String _name, String _api, String _max, int _device, String _type) {
 	name		= _name;
 	api			= _api;
 	max			= _max;
 	device		= _device;
-	type		= _type;	
+	type		= _type;
+	value		= 0;
+	
+	display		= { OFF, OFF, OFF, OFF, OFF, OFF,
+		 			OFF, OFF, OFF, OFF, OFF, OFF,
+					OFF, OFF, OFF, OFF, OFF, OFF };
+	
+	bar = Adafruit_24bargraph();
+	bar.begin(0x70+device);
 }
 
 // ===================
@@ -56,12 +59,28 @@ void Bargraph::print() {
 // ====================
 
 void Bargraph::format() {
+	for (int i=0; i<24; i++) {
+		bar.setBar(i, 0);
+	}
 	
-	
+	if (type == "Default") {
+		float percent = (value * 100.0) / max;
+		int color = 0;
+		
+		if (p > 50)				{	color = green;	}
+		if (p <= 50 && p > 20)	{	color = yellow;	}
+		if (p <= 20 && p > 0)	{	color = red;	}
+		
+		for (int i=0; i<int(percent); i++) {
+			display[i] = color;
+		}
+	}
 }
 
 void Bargraph::write() {
-	
-	
-	
+	for (int i=0; i<24; i++) {
+		bar.setBar(i, display[i]);
+	}
+
+    bar.writeDisplay();
 }
--- arduino/libraries/Bargraph/bargraph.h
+++ arduino/libraries/Bargraph/bargraph.h
@@ -14,23 +14,24 @@ class Bargraph {
     String name;
 	String api;
 	
-	Bargraph(String _name, String _api, String _max, int _device);
-	Bargraph(String _name, String _api, String _max, int _device, String _type);
+	Bargraph(String _name, String _api, String _max, int _device, String _type="Default");
 	
 	void set(long _value);
 	void update();
     void print();
   
   private :
-	 long max;
-	 long value;
-	 int device;
-	 int size;
-	 String type;
+  	Adafruit_24bargraph bar;
+	int display[24];
+  
+	long max;
+	long value;
+	int device;
+	int size;
+	String type;
 	 
-	 
-	 void format();
-	 void write();
+	void format();
+	void write();
 };
 
 #endif

2e190f7d675666f37fdd7a83d3ea4e7537833163
lawnmowerlatte <lawnmowerlatte@gmail.com>
2015-04-23 23:28:56 -0400

    Fixed various problems for Windows

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ try:
     import sys
     import os
     import platform
-    import serial
     import time
     import datetime
     import socket
@@ -18,6 +17,8 @@ except:
 
 debugger = 6
 usecolor = False
+tryinstall = False
+
 def debug(message=None, level=debugger, newline=True, color=None):
     """Debugging log message"""
     
@@ -48,88 +49,145 @@ def fail(message=None, end=True):
         else:
             debug(message, newline=False)
 
+# If termcolor is already installed, use it from the start
+# We'll try to install it later if not...
+if platform.system() != 'Windows':
+    try:
+        import termcolor
+        usecolor = True
+    except:
+        pass
+
 debug("Checking Python version: ", newline=False)
 if ("2.7" in sys.version.partition(' ')[0]):
     ok()
 else:
-    fail("Please install Python 2.7")
+    fail("Please use Python 2.7")
 
-debug("Checking for pip: ", newline=False)
-if os.system("which pip 2>&1 > /dev/null") == 0:
-    ok()
-else:
-    fail("Please install pip")
-    
+# Check for pip
 debug("Importing pip: ", newline=False)
 try:
     import pip
-except:
-    fail("Unable to import pip for installation")
-else:
+    tryinstall = True
     ok()
-    
-debug("Checking for termcolor: ", newline=False)
-try:
-    import termcolor
-    usecolor = True
 except:
+    fail("Not found", False)
+    debug("Setup will continue, but will not be able to install packages automatically. Setup will fail if packages are missing.")
+    
+# Opporunistically use termcolor if available
+if platform.system() != 'Windows':
+    debug("Checking for termcolor: ", newline=False)
     try:
-        fail("Installing termcolor: ", False)
-        pip.main('install', 'termcolor')
-    except:
-        fail("Not required, continuing.", False)
-        debug()
-    else:
+        import termcolor
+        usecolor = True
         ok()
-else:
-    ok()
+    except:
+        fail("Not found", False)
+        
+        if tryinstall:
+            debug("Installing termcolor: ", newline=False)
+            try:
+                pip.main("install", "termcolor")
+                import termcolor
+                ok()
+            except:
+                fail("Not required, continuing.", False)
+        else:
+            debug("Termcolor is not required, continuing.")
     
+# Check for PySerial
+debug("Checking for PySerial: ", newline=False)
+try:
+    import serial
+    ok()
+except:
+    fail("Not found", False)
+    
+    if tryinstall:
+        debug("Installing PySerial: ", newline=False)
+        try:
+            pip.main("install", "pyserial")
+            import serial
+            ok()
+        except:
+            fail("Installation failed", False)
+            fail("Please install pyserial using pip.")
+    else:
+        fail("Please install pyserial using pip.")
+
+# Check for serial toolchain
 debug("Checking for serial tools: ", newline=False)
 if platform.system() == 'Windows':
     try:
         import _winreg as winreg
         import itertools
-    except:
-        fail("Please install winreg and itertools")
-    else:
         ok()
+    except:
+        fail("Not found", False)
+        if tryinstall:
+            debug("Installing winreg and itertools: ", newline=False)
+            try:
+                pip.main("install", "winreg")
+                pip.main("install", "itertools")
+                import _winreg as winreg
+                import itertools
+                ok()
+            except:
+                fail("Installation failed", False)
+                fail("Please install winreg and itertools using pip.")
+        else:
+            fail("Please install winreg and itertools using pip.")
+
 elif platform.system() == 'Darwin':
     try:
         from serial.tools import list_ports
-    except:
-        fail("Please install serial.tools")
-    else:
         ok()
+    except:
+        fail("Not found", False)
+        fail("Please install serial.tools using pip.")
+
 else:
     try:
         import glob
-    except:
-        fail("Please install glob")
-    else:
         ok()
+    except:
+        fail("Not found", False)
+        if tryinstall:
+            debug("Installing glob: ", newline=False)
+            try:
+                pip.main("install", "glob")
+                import glob
+                ok()
+            except:
+                fail("Installation failed", False)
+                fail("Please install winreg and itertools using pip.")
+        else:
+            fail("Please install glob using pip.")
         
 debug("Checking for pyksp: ", newline=False)
 try:
     sys.path.append("./pyksp")
     import pyksp
+    ok()
 except:
-    if platform.system() is not "Windows":
-        fail("Trying to install pyksp: ", False)
+    fail("Not found")
+    if platform.system() != "Windows":
+        debug("Trying to install pyksp: ", newline=False)
         try:
             if os.system("which git 2>&1 > /dev/null") == 0:
                 if os.system("git clone -q " + pyksp_git + "  2>&1 > /dev/null") == 0:
                     sys.path.append("./pyksp")
                     import pyksp
+                    ok()
+                else:
+                    fail("Clone failed.", False)
             else:
-                fail("Couldn't find git")
+                fail("Couldn't find git", False)
         except:
-            fail("Please install pyksp within the KAPCOM directory")
-        else:
-            ok()
+            fail("Installation failed", False)
     else:
-        fail("Please install pyksp within the KAPCOM directory")
-else:
-    ok()
+        fail("Please install pyksp within the KAPCOM directory using git", False)
+        fail("git clone -q " + pyksp_git)
     
 debug()
 debug("Prerequisites met. Please run configure.py", color="green")
